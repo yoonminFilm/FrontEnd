8b0483de237c755ab14b60190e3c2277817cc8c7
SungMin Yoon <yoonmin.tech@gmail.com>
2025-03-04 20:20:12 +0900

    feat: api security service and edit internal introductions to about page

--- src/features/aboutPage/components/AboutText.js
+++ src/features/aboutPage/components/AboutText.js
@@ -5,25 +5,25 @@ const AboutText = ()=>{
     return(
         <>
             <div className="about-page-text">
-            <h6>Sungmin Yoon is a photographer based in Korea.<br /></h6>
+            <h6><b>Sungmin Yoon is a photographer based in Korea.</b></h6>
+            소프트웨어 개발자로 윤민 필름 계정을 운영 중이며 <br/> '사랑'과 '도시의 패턴'을 주제로 사진 및 영상 활동을 하고있다.<br /><br />
 
-            현재 소프트웨어 개발자로 취업을 준비하며 <br/>윤민필름으로 사진 및 영상 작업 활동을하고있다.<br /><br />
-
-            <strong> yoonmin_film</strong>
-            사진에서 ‘사랑’을 중점으로 자그마한 사진들을 담아내고 글로 풀어 나가고 있다.<br /><br />
+            <strong>Original work</strong>
+            사진과 글로 다양한 형태의 '사랑'을 담아 낸다.<br /><br />
             <strong>Urban pattern</strong>
-            매번 반복되는 지겨운 일상의 도시를 다른 빛으로 매일을 담아내며<br />
-            우리가 살아가는 공간이 지겨운 반복이 아닌,<br />
-            숨을 쉬고 유기적으로 움직이는 모습들에서 <br />
-            매일의 다름은 담아내며 '삶, 살아냄'을 표현하는데 집중한다.<br />
+            매번 반복되는 지겨운 일상의 도시 빌딩 숲 사이에 내려 앉는 빛들을 담아낸다.<br />
+            우리가 살아가는 공간은 톱니 바퀴가 아닌 <br />
+            유기적으로 살아 숨 쉬며 도시 속에서 '삶과 살아냄'을 표현하고자한다.<br />
+            <br />
+            <strong>Youtube(Film&Edit)</strong>
+            FDA(FriendlyDarkArmy), 삭개오 워십<br />
             <br />
             <strong>History:</strong>
             2024 후지필름 포토페스타 전시에서  단사진 작가<br />
-            2024 에어로케이&필름로그 삿포로 시티 에디터로 활동<br />
-            유튜브 팀 | FDA, 삭개오 워십 <br />
+            2024 에어로케이&필름로그 삿포로 시티 에디터<br />
             <br />
             <strong>Featured in:</strong>
-            <strong>Fujifilm, 월간 사진, AeroK & Flimlog, Classics Film Club</strong>
+            Fujifilm, 월간 사진, AeroK & Flimlog, Classics Film Club
 
             </div>
         </>
