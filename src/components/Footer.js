613768011b52aed68e864a2f5b2d495f063c716d
SungMin Yoon <yoonmin.tech@gmail.com>
2025-03-05 18:19:28 +0900

    style: footer, nav 모바일 및 웹 환경 반응형 사이트 제작 + 아이콘 변경

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,28 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import emailIcon from '../assets/images/square-email.svg';
-import instagramIcon from '../assets/images/square-instagram.svg';
-import youtubeIcon from '../assets/images/square-youtube.svg';
+import emailIcon from '../assets/images/email-icons.svg';
+import instagramIcon from '../assets/images/instagram-logo.png';
+import youtubeIcon from '../assets/images/youtube-logo.png';
+import './components.css';
 
 function FooterComponents() {
   return (
     <footer>
       <Container>
           <Row className="justify-content-md-center">        
-            <Col xs sm={4} className='justify-content-md-start  align-items-end'>
-            <h2 className='footer-text'>© 2024 Sungmin Yoon All rights reserved.</h2>
+            <Col xs={5} sm={4} className='justify-content-md-start  align-items-end'>
+            <h2 className='footer-text'>© 2024 SungminYoon All rights reserved.</h2>
             </Col>
-            <Col xs sm={2}>
+            <Col xs={1} sm={2}>
             </Col>
-            <Col xs sm={2}>
+            <Col xs={2} sm={2}>
               <a href='mailto:ysmgg1@naver.com' target='_blank'><img src={emailIcon} alt="email icon" className='footer-svg'/></a>
             </Col>
-            <Col xs sm={2}>
+            <Col xs={2} sm={2}>
               <a href='https://www.instagram.com/yoonmin_film/' target='_blank'><img src={instagramIcon} alt="email icon" className='footer-svg'/></a>
             </Col>
-            <Col xs sm={2}>
+            <Col xs={2} sm={2}>
               <a href='https://www.youtube.com/@friendly-dark-army' target='_blank'><img src={youtubeIcon} alt="email icon" className='footer-svg'/></a>
             </Col>
           </Row>          
--- src/features/aboutPage/components/AboutText.js
+++ src/features/aboutPage/components/AboutText.js
@@ -5,26 +5,38 @@ const AboutText = ()=>{
     return(
         <>
             <div className="about-page-text">
-            <h6><b>Sungmin Yoon is a photographer based in Korea.</b></h6>
-            소프트웨어 개발자로 윤민 필름 계정을 운영 중이며 <br/> '사랑'과 '도시의 패턴'을 주제로 사진 및 영상 활동을 하고있다.<br /><br />
-
-            <strong>Original work</strong>
-            사진과 글로 다양한 형태의 '사랑'을 담아 낸다.<br /><br />
-            <strong>Urban pattern</strong>
-            매번 반복되는 지겨운 일상의 도시 빌딩 숲 사이에 내려 앉는 빛들을 담아낸다.<br />
-            우리가 살아가는 공간은 톱니 바퀴가 아닌 <br />
-            유기적으로 살아 숨 쉬며 도시 속에서 '삶과 살아냄'을 표현하고자한다.<br />
-            <br />
-            <strong>Youtube(Film&Edit)</strong>
-            FDA(FriendlyDarkArmy), 삭개오 워십<br />
-            <br />
-            <strong>History:</strong>
-            2024 후지필름 포토페스타 전시에서  단사진 작가<br />
-            2024 에어로케이&필름로그 삿포로 시티 에디터<br />
-            <br />
-            <strong>Featured in:</strong>
-            Fujifilm, 월간 사진, AeroK & Flimlog, Classics Film Club
-
+                <h6><b>Sungmin Yoon is a photographer based in Korea.</b></h6>
+                소프트웨어 개발자로 윤민 필름 계정을 운영 중이며 <br/> '사랑'과 '도시의 패턴'을 주제로 사진 및 영상 활동을 하고있다.
+                <hr class="hr-divider"></hr>
+                <ul>
+                    <h6><b>⌜Project⌟</b></h6>
+                    <li>
+                        <strong> Original</strong><br/>
+                        다양한 형태의 '사랑'을 찍고 써내려가며 일상의 기록을 남긴다.
+                    </li>
+                    <li>
+                        <strong> Urban pattern, Reflect cities</strong><br/>
+                        매번 반복되는 지겨운 일상의 도시 빌딩 숲 사이에 내려 앉는 빛들을 담아낸다.<br/>
+                        공간의 반복이 아닌 유기적으로 살아 숨 쉬는 도시를 표현하고자한다.
+                    </li>
+                    <li>
+                        <strong> Filming & Editing(Youtube)</strong><br/>
+                        <a href="https://www.youtube.com/@friendly-dark-army">FDA(FriendlyDarkArmy)</a><br />
+                        <a href="https://www.youtube.com/@zacchaeus.worship">삭개오 워십(zacchaeus worship)</a>
+                    </li>
+                </ul>
+                <hr class="hr-divider"></hr>
+                <h6><b>⌜History⌟</b></h6>
+                <a href="https://nodeul.org/program/%ED%9B%84%EC%A7%80%ED%95%84%EB%A6%84-%ED%8F%AC%ED%86%A0%ED%8E%98%EC%8A%A4%ED%83%80-2024%E3%80%88%EC%B2%9C-%EA%B0%9C%EC%9D%98-%EA%BF%88%E3%80%89/" target="_blank">
+                    2024 후지필름 포토페스타 전시에서  단사진 작가
+                </a>
+                <a href="https://brand.aerok.net/cityeditor_sapporo" target="_blank">
+                    2024 에어로케이&필름로그 삿포로 시티 에디터
+                </a>
+                <hr class="hr-divider"></hr>
+                <h6><b>⌜Featured in⌟</b></h6>
+                Fujifilm, Monthlyphoto(월간사진), Classics Film Club, AeroK & Flimlog
+                <br/>
             </div>
         </>
     );
